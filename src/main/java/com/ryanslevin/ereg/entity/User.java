48d9a074649449391841470c44d1a7f3ccfdeafe
ryanslevin <ryanslevin@gmail.com>
2019-06-29 17:33:43 -0700

    Added user entity
    User entity added to Entity folder. Users will start with id, firstName, lastName, birthDate, and gender.
    Added entitiy and table annotations and linked to table, Column annotations added to fields. Added
    getters/setters and constructors.

---
+++ src/main/java/com/ryanslevin/ereg/entity/User.java
@@ -0,0 +1,83 @@
+package com.ryanslevin.ereg.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private String id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="birth_date")
+    private Date birthDate;
+
+    @Column(name="gender")
+    private String gender;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public User(String id, String firstName, String lastName, Date birthDate, String gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+    }
+
+    
+
+
+
+
+
+}
