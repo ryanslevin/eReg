85e5cd7bda7ae3a01843b8438f442f2b202e0467
ryanslevin <ryanslevin@gmail.com>
2019-07-03 16:53:08 -0700

    Added course entitiy and linked to courses table in db

--- front-end/react-app/src/App.js
+++ front-end/react-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MAKE A FRONT END
         </a>
       </header>
     </div>
---
+++ src/main/java/com/ryanslevin/ereg/entity/Courses.java
@@ -0,0 +1,137 @@
+package com.ryanslevin.ereg.entity;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="courses")
+public class Courses {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="start_date")
+    private Date startDate;
+
+    @Column(name="end_date")
+    private Date endDate;
+
+    @Column(name="start_time")
+    private Time startTime;
+
+    @Column(name="end_time")
+    private Time endTime;
+
+    @Column(name="location")
+    private String location;
+
+    @Column(name="instructor")
+    private String instructor;
+
+    public Courses() {
+
+    }
+
+
+    public Courses(String name, Date startDate, Date endDate, Time startTime, Time endTime, String location,
+            String instructor) {
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.location = location;
+        this.instructor = instructor;
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return "Courses [endDate=" + endDate + ", endTime=" + endTime + ", id=" + id + ", instructor=" + instructor
+                + ", location=" + location + ", name=" + name + ", startDate=" + startDate + ", startTime=" + startTime
+                + "]";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    
+
+    
+
+    
+
+
+
+
+}
